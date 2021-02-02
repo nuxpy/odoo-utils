db97cbdfea9b4f5e8ff006f26963a08f25f5c927
nuxpy <nuxpy4@gmail.com>
2021-02-02 10:45:54 +0100

    Carga inicial OCA modules for Odoo 11.

---
+++ modulos-terceros/install-ocamods-o11.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' This script gets modules from OCA repository on GitHub to version 13
+    * At first time installs "aptitude" as main manager of installations
+    * You can use it on Debian/Ubuntu as root user or sudo way:
+    
+        chmod +x install-ocamods-o13.py
+        ./install-ocamods-o13.py
+
+'''
+import os, re, sys
+
+paqrepdeb = "git"
+ocaurl = 'https://github.com/OCA/'
+branch = '11.0'
+repositories = [
+    'account-closing',
+    'account-consolidation',
+    'account-financial-tools',
+    'account-payment',
+    'account-reconcile',
+    'bank-payment',
+    'bank-statement-import',
+    'brand',
+    'calendar',
+    'connector-ecommerce',
+    'crm',
+    'e-commerce',
+    'helpdesk',
+    'hr',
+    'hr-attendance',
+    'hr-expense',
+    'hr-holidays',
+    'l10n-spain',
+    'project',
+    'stock-logistics-barcode',
+    'stock-logistics-reporting',
+    'stock-logistics-tracking',
+    'stock-logistics-transport',
+    'stock-logistics-warehouse',
+    'stock-logistics-workflow',
+    'timesheet',
+    'vertical-hotel',
+    'web',
+    'website',
+    'website-cms',
+    'website-themes'
+]
+
+if not os.path.exists('oca'):
+    os.system('apt-get install aptitude')
+    os.system('aptitude update')
+    os.system('aptitude -yr install %s' % (paqrepdeb))
+    os.mkdir('oca')
+    for r in repositories:
+        os.system('cd oca; git clone -b %s %s%s' % (branch, ocaurl, r))
+else:
+    for r in repositories:
+        if not os.path.exists('oca/%s' % r):
+            os.mkdir('oca/%s' % r)
+            os.system('cd oca; git clone -b %s %s%s; cd -' % (branch, ocaurl, r))
+        else:
+            os.system('cd oca/%s; git pull; cd -' % (r))
+
